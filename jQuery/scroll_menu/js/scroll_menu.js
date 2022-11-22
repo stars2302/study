1b3541ffa2a6f2386fea25a91fba487cc082b8d4
stars2302 <stars2302@naver.com>
2022-11-22 17:12:58 +0900

    20 days study

---
+++ jQuery/scroll_menu/js/scroll_menu.js
@@ -0,0 +1,38 @@
+$(function(){
+  var 
+  $menu = $('#top_menu ul li'),
+  $contents = $('#contents > div');
+  console.log($menu);
+
+  
+  $menu.click(function(x){
+    x.preventDefault();
+    
+    //$menu를 클릭하면 해당 요소에 Class 'on' 이 추가
+    // $(this).addClass('on').siblings().removeClass('on');
+    
+    //$menu를 클릭하면 상충되는 section으로 이동하기
+    var idx = $(this).index();
+    var section = $contents.eq(idx);
+    var sectionDistance = section.offset().top;
+    $('html,body').stop().animate({scrollTop:sectionDistance});
+  });
+
+  //scroll했을 때, $menu의 class 'on'이 추가되지 않은 오류해결
+  $(window).scroll(function(){
+    $contents.each(function(){
+      if($(this).offset().top <= $(window).scrollTop()){
+        var idx = $(this).index();
+        $menu.removeClass('on');
+        $menu.eq(idx).addClass('on')
+      }
+    });
+  });
+
+
+
+
+
+});//document
+
+
