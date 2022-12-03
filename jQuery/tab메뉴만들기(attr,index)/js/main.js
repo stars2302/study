a39d6b268e612a2ae837b6fb4bf78e32d1dedd69
stars2302 <stars2302@naver.com>
2022-12-03 14:56:17 +0900

    21 days study

--- jQuery/tab메뉴만들기(attr,index)/js/main.js
+++ jQuery/tab메뉴만들기(attr,index)/js/main.js
@@ -27,6 +27,7 @@ $(function () {
 
   // 2. index 활용하기
   //링크를 클릭하면 할 일
+  /*
   tab.click(function(e){
     e.preventDefault();
 
@@ -47,8 +48,13 @@ $(function () {
   //.trigger('event') - 방아쇠를 당기다라는 뜻. 강제로 뭔가를 시킬 때 활용
   // 3. 새로고침을 하면 5번째가 먼저 보여지는 오류 해결
   tabAnchor.eq(0).trigger('click');
+  */
   
-  
+  // 3. jqueryui.com -> Widgets -> tabs 활용
+  $('.work-section').tabs({
+    show : { duration: 300 },
+    hide : { duration: 300 }
+  });
 
 });
 
